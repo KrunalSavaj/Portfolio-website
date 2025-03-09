db2c9612e111f624e5b98c6e3625249d89996014
KrunalSavaj <developer.krunalsavaj@gmail.com>
2025-03-09 12:00:59 +0530

    Remove github bug file and change resume

--- src/lib/content/footer.ts
+++ src/lib/content/footer.ts
@@ -2,6 +2,6 @@ import { FooterSectionType } from '@/lib/types/sections';
 
 export const footerSection: FooterSectionType = {
   title: 'Design & Built by Krunal Savaj',
-  link: 'https://github.com/vatsalsinghkv/portfolio-website',
+  link: 'https://github.com/KrunalSavaj/Portfolio-website/',
 };
 
